f36d07e17b68aede06d8a999da3635f57631257d
Abhay Singh Rathore <69167518+AbhaySinghRathore@users.noreply.github.com>
2020-08-27 13:04:03 +0530

    Add files via upload

--- app/src/main/java/android/example/foodapp/MainActivity.java
+++ app/src/main/java/android/example/foodapp/MainActivity.java
@@ -1,14 +1,31 @@
-package android.example.foodapp;
+package com.example.agricultureapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton=(Button) findViewById(R.id.startBtn);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
