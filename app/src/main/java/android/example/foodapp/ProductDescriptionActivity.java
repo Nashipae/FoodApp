1a3bedb0b4e944a84f005014613e2a1b3cc7d620
AbhaySinghRathore <abhaysinghrathore5933@gmail.com>
2020-09-07 14:26:20 +0530

    Prodcut Description Layout Added

---
+++ app/src/main/java/android/example/foodapp/ProductDescriptionActivity.java
@@ -0,0 +1,13 @@
+package android.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_description);
+    }
+}
