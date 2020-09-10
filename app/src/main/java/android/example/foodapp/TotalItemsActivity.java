2be77cb149de5658f4ed145440e7ed5ababfe092
AbhaySinghRathore <abhaysinghrathore5933@gmail.com>
2020-09-10 08:07:36 +0530

    TotalProductActivity Added and Some Minor Changes

---
+++ app/src/main/java/android/example/foodapp/TotalItemsActivity.java
@@ -0,0 +1,14 @@
+package android.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TotalItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_total_items);
+    }
+}
