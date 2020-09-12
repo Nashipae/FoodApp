d935b80808cc8ff404886f331b9c7832199e6365
AbhaySinghRathore <abhaysinghrathore5933@gmail.com>
2020-09-12 05:43:38 +0530

    Payment Mode Activity Added

---
+++ app/src/main/java/android/example/foodapp/PaymentModeActivity.java
@@ -0,0 +1,14 @@
+package android.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_mode);
+    }
+}
--- app/src/main/java/android/example/foodapp/PersonInformationActivity.java
+++ app/src/main/java/android/example/foodapp/PersonInformationActivity.java
@@ -9,10 +9,20 @@ import android.widget.Button;
 
 public class PersonInformationActivity extends AppCompatActivity {
 
+    private Button continueBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_information);
+
+        continueBtn=findViewById(R.id.continueBtn);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(PersonInformationActivity.this,PaymentModeActivity.class);
+                startActivity((intent));
+            }
+        });
     }
 }
