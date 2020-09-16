dab11d1f9fd71afafb5a69a959778806221c832c
Dipanshu <dipanshu231099@gmail.com>
2020-09-16 21:52:10 +0530

    prototype backend completed

--- app/src/main/java/android/example/foodapp/AdminTrackOrdersActivivty.java
+++ app/src/main/java/android/example/foodapp/AdminTrackOrdersActivivty.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -38,16 +39,130 @@ public class AdminTrackOrdersActivivty extends AppCompatActivity {
     private FirebaseRecyclerOptions<Order> options;
     private FirebaseRecyclerAdapter<Order, AdminActiveOrdersViewHolder> adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private Button active,processing,delivered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_delivery_history);
+        setContentView(R.layout.activity_admin_track_orders_activivty);
 
         recyclerView = findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        active = findViewById(R.id.activeOrders);
+        processing = findViewById(R.id.processingOrders);
+        delivered = findViewById(R.id.deliveredOrders);
 
+        active.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showOrders(0);
+            }
+        });
+
+        processing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showOrders(1);
+            }
+        });
+
+        delivered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showOrders(2);
+            }
+        });
+
+    }
+
+    private void showOrders(final int status) {
+        final DatabaseReference OrderRef = FirebaseDatabase.getInstance().getReference().child("Orders").child("Admin View").child("Products");
+        final DatabaseReference userOrderRef = FirebaseDatabase.getInstance().getReference().child("Orders").child("User View");
+        Query query;
+        if(status==0) query = OrderRef.orderByChild("status").equalTo("Order Booked");
+        else if(status==1) query = OrderRef.orderByChild("status").equalTo("Processing");
+        else query = OrderRef.orderByChild("status").equalTo("Delivered");
+
+        options = new FirebaseRecyclerOptions.Builder<Order>()
+                .setQuery(query, Order.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<Order, AdminActiveOrdersViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final AdminActiveOrdersViewHolder holder, int position, @NonNull final Order model) {
+
+                DatabaseReference product = FirebaseDatabase.getInstance().getReference().child("Products").child(model.getPid());
+                product.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        holder.name.setText(snapshot.child("prod_name").getValue().toString());
+                        holder.desc.setText(snapshot.child("prod_desc").getValue().toString());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {}
+                });
+
+                holder.userID.setText(model.getUser());
+                holder.userAddress.setText(model.getAddress());
+                holder.total.setText(model.getTotal());
+                holder.price.setText(model.getPrice());
+                holder.quantity.setText(model.getQuantity());
+                holder.status.setText(model.getStatus());
+                holder.deliveryDate.setText(model.getOrderReceivedDate());
+                holder.orderDate.setText(model.getOrderOnDate());
+                if(status==0) holder.accept.setText("Accept Order");
+                else if(status==1) holder.accept.setText("Mark Delivered");
+                else holder.accept.setText("View Active Orders");
+                holder.accept.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(status==0){
+                            OrderRef.child(model.getOrderID()).child("status").setValue("Processing");
+                            userOrderRef.child(model.getUser()).child(model.getOrderID()).child("status").setValue("Processing");
+                            Toast.makeText(AdminTrackOrdersActivivty.this, "updated status", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(status==1){
+                            OrderRef.child(model.getOrderID()).child("status").setValue("Delivered");
+                            userOrderRef.child(model.getUser()).child(model.getOrderID()).child("status").setValue("Delivered");
+                            Toast.makeText(AdminTrackOrdersActivivty.this, "updated status", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            showOrders(0);
+                        }
+                    }
+                });
+                if(status==0 || status==1)holder.reject.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        OrderRef.child(model.getOrderID()).child("status").setValue("Rejected");
+                        userOrderRef.child(model.getUser()).child(model.getOrderID()).child("status").setValue("Rejected");
+                        Toast.makeText(AdminTrackOrdersActivivty.this, "updated status", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                else {
+                    holder.reject.setText("View Processing Orders");
+                    holder.reject.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            showOrders(1);
+                        }
+                    });
+                }
+
+
+            }
+
+            @NonNull
+            @Override
+            public AdminActiveOrdersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.admin_active_orders_item,parent,false);
+                AdminActiveOrdersViewHolder holder = new AdminActiveOrdersViewHolder(view);
+                return holder;
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
     }
 
     @Override
